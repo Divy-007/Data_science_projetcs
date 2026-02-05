43f93934834ee375773908bc96805fdd26b3abc4
Divy kushwaha <work.divy07@gmail.com>
2026-02-05 22:20:39 +0530

    improve logic customer_id -> customer_unique_id

--- ecommerce-anayltics/sql/order_customer_profit.sql
+++ ecommerce-anayltics/sql/order_customer_profit.sql
@@ -1,7 +1,10 @@
+DROP VIEW IF EXISTS ecommerce.order_customer_profit;
 CREATE OR REPLACE VIEW ecommerce.order_customer_profit AS
-
-select o.customer_id,
+select c.customer_unique_id,o.customer_id,
 op.order_id,op.freight_value,op.order_net_profit
-from ecommerce.orders o
+from ecommerce.customers c
+left join ecommerce.orders o 
+on c.customer_id=o.customer_id
 left join ecommerce.order_profit op
 on o.order_id=op.order_id;
+
